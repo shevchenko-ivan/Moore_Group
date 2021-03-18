756e6a1c4033a8bdd2500bf8bb214e208afe9754
Shevchenko__Ivan <shevchenkoivan1951@gmail.com>
2021-03-18 17:28:03 +0200

    Add-burger

---
+++ js/script-b.js
@@ -0,0 +1,6 @@
+$('.burger, .cl, .overlay').click(function(){
+  $('.burger').toggleClass('clicked');
+  $('.overlay').toggleClass('show');
+  $('nav').toggleClass('show');
+  $('body').toggleClass('overflow');
+}); 
