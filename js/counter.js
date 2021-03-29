88b4176984a8ebe7592e7096085b00a761708c86
Shevchenko__Ivan <shevchenkoivan1951@gmail.com>
2021-03-29 12:31:09 +0300

    End

--- js/counter.js
+++ js/counter.js
@@ -1,4 +1,28 @@
-window.addEventListener('scroll',function(e){let lastEl=document.getElementById('qw')
-console.log('lastEl',lastEl.offsetTop)
-console.log('window.scrollY',window.scrollY)
-if(window.scrollY>lastEl.offsetTop-1000){$('.counter').each(function(){var $this=$(this),countTo=$this.attr('data-count');$({countNum:$this.text()}).animate({countNum:countTo},{duration:1000,easing:'linear',step:function(){$this.text(Math.floor(this.countNum))},complete:function(){$this.text(this.countNum)}})})}})
+window.addEventListener('scroll', function () {
+
+  let lastEl = document.getElementById('qw')
+  console.log('lastEl', lastEl.offsetTop)
+  console.log('window.scrollY', window.scrollY)
+
+  if (window.scrollY > lastEl.offsetTop - 1000) {
+    $('.counter').each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      })
+      .animate({
+        countNum: countTo
+      }, {
+        duration: 1000,
+        easing: 'linear',
+        step: function () {
+          $this.text(Math.floor(this.countNum))
+        },
+        complete: function () {
+          $this.text(this.countNum)
+        }
+      })
+    })
+  }
+})
